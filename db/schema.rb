5bdc3d2d659bd790bf6d2a2bd37228b5c5146d11
jeanjj <jhbarbecormier@gmail.com>
2015-04-05 13:49:13 +0200

    emergency on earth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319223334) do
+ActiveRecord::Schema.define(version: 20150404175704) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150319223334) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
