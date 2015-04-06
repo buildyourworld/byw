b32a2d0245fa26c0855427659ff37084aeb2bb92
jeanjj <jhbarbecormier@gmail.com>
2015-04-06 13:12:23 +0200

    des petits trucs

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150405135210) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
